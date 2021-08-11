e68809136b9df7a906d7633e74365e25e564e95a
Matt Pennell <11513260+mattp256@users.noreply.github.com>
2021-08-10 23:16:43 -0500

    Couple touch ups to test script

--- test/serial2ros.py
+++ test/serial2ros.py
@@ -3,8 +3,8 @@ from serial import Serial
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
-WHEELE_MAX_LINEAR = 1
-WHEELE_MAX_ANGULAR = 1
+WHEELE_MAX_LINEAR = .2 #1
+WHEELE_MAX_ANGULAR = .52 #1
 
 X_MIN_SPEED = 122
 X_MAX_SPEED = 305
@@ -23,11 +23,13 @@ def publish_message():
     rate = rospy.Rate(100) # 100hz
     twist = Twist()
     while not rospy.is_shutdown():
+    	# Read touchscreen coords over USB
         coords = comm.readline()
         try:
             decoded_bytes = coords[0:len(coords)-2].decode("utf-8") # remove newline
             strbytes = decoded_bytes.split(',')
             try:
+            	# Convert coordinates to cmd_vel format ("Twist" - linear + angular speed)
                 x = float(strbytes[0])
                 y = float(strbytes[1])
                 twist.linear.x = ((-(y - Y_ZERO_SPEED))/Y_SPEED_NORMALIZE)*WHEELE_MAX_LINEAR
@@ -36,7 +38,7 @@ def publish_message():
                 if(twist.linear.x < 0):
                     twist.angular.z = -twist.angular.z
                     
-                print(twist.linear.x,twist.angular.z)
+                # Publish!
                 pub.publish(twist)
             except:
                 pass
